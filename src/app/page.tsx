4dda8fa1b510d185c263315dabf3b11b7614a31f
Adway Lachhiramka <66119557+adway7103@users.noreply.github.com>
2024-05-22 16:21:48 +0530

    ok

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Hero } from "./pages/Home/Hero";
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-     <Hero/>
-    </main>
-  );
-}
