56cc3c32a17b22009f76eb1a0cc5a3c5b45b30ce
Adway Lachhiramka <66119557+adway7103@users.noreply.github.com>
2024-05-22 17:20:29 +0530

    ok

---
+++ components/ui/header.tsx
@@ -0,0 +1,53 @@
+import Link from 'next/link'
+import MobileMenu from './mobile-menu'
+
+export default function Header() {
+  return (
+    <header className="absolute w-full z-30">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="flex items-center justify-between h-20">
+          {/* Site branding */}
+          <div className="shrink-0 mr-4 font-bold text-[1.6rem]">
+            {/* Logo */}
+            <Link href="/" className="block" aria-label="Cruip">
+              Technotribes
+            </Link>
+          </div>
+
+          {/* Desktop navigation */}
+          <nav className="hidden md:flex md:grow">
+            {/* Desktop sign in links */}
+            <ul className="flex grow justify-end flex-wrap items-center">
+              <li>
+                <Link
+                  href="/signin"
+                  className="font-medium text-white hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out"
+                >
+                  Our Technologies
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/signin"
+                  className="font-medium text-white hover:text-gray-200 px-4 py-3 flex items-center transition duration-150 ease-in-out"
+                >
+                  Portfolio
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/signup"
+                  className="btn-sm text-white ml-3"
+                >
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+          <MobileMenu />
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,119 @@
+const svgToDataUri = require("mini-svg-data-uri");
+
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          100: "#EBF1F5",
+          200: "#D9E3EA",
+          300: "#C5D2DC",
+          400: "#9BA9B4",
+          500: "#707D86",
+          600: "#55595F",
+          700: "#33363A",
+          800: "#25282C",
+          900: "#151719",
+        },
+        purple: {
+          100: "#F4F4FF",
+          200: "#E2E1FF",
+          300: "#CBCCFF",
+          400: "#ABABFF",
+          500: "#8D8DFF",
+          600: "#5D5DFF",
+          700: "#4B4ACF",
+          800: "#38379C",
+          900: "#262668",
+        },
+      },
+      spacing: {
+        "9/16": "56.25%",
+        "3/4": "75%",
+        "1/1": "100%",
+      },
+      fontFamily: {
+        inter: ["var(--font-inter)", "sans-serif"],
+        "architects-daughter": [
+          "var(--font-architects-daughter)",
+          "sans-serif",
+        ],
+      },
+      fontSize: {
+        xs: "0.75rem",
+        sm: "0.875rem",
+        base: "1rem",
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
+        "3xl": "2rem",
+        "4xl": "2.5rem",
+        "5xl": "3.25rem",
+        "6xl": "4rem",
+      },
+      inset: {
+        full: "100%",
+      },
+      letterSpacing: {
+        tighter: "-0.02em",
+        tight: "-0.01em",
+        normal: "0",
+        wide: "0.01em",
+        wider: "0.02em",
+        widest: "0.4em",
+      },
+      minWidth: {
+        10: "2.5rem",
+      },
+      scale: {
+        98: ".98",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    addVariablesForColors,
+    function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "bg-grid": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-grid-small": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`,
+          }),
+          "bg-dot": (value) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
+};
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
